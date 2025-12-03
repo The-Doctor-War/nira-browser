cc03735e48c3d8ddbd633e08b16fb731e48b746f
Priyanshu Rai <idealpriyanshu@gmail.com>
2025-12-03 15:28:21 +0530

    Remove jetifier and remove some translations, fixes, update version string.

--- app/src/main/java/com/prirai/android/nira/webapp/WebAppNotificationManager.kt
+++ app/src/main/java/com/prirai/android/nira/webapp/WebAppNotificationManager.kt
@@ -1,9 +1,12 @@
 package com.prirai.android.nira.webapp
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.prirai.android.nira.R
@@ -78,6 +81,20 @@ class WebAppNotificationManager(private val context: Context) {
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setAutoCancel(true)
 
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
         notificationManager.notify(notificationId, builder.build())
     }
 
